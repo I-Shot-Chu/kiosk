caae188014bdee29a6417cb724c23b9763be8514
temless <ljw980722@naver.com>
2024-10-07 13:00:31 +0900

    lee

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -6,16 +6,15 @@ import Nav from "../navbar/Nav";
 
 const Layout = ()=>{
     return(
-<<<<<<< HEAD
-        <>  
-            <Nav/>
-=======
+
+
+
 
 
          <>
              <Nav/>
 
->>>>>>> 8e87495d2f1774c9acb136b8e3a1a415731f063e
+
             <Outlet/>
         </>
     )
--- src/navbar/Nav.js
+++ src/navbar/Nav.js
@@ -5,19 +5,7 @@ const Nav = ()=>{
     return(
         <div>
             <ul>
-<<<<<<< HEAD
-                <NavLink to={"/menu/hotcoffee"}>Hot Coffee</NavLink>
-                <NavLink to={"/menu/icecoffee"}>Ice Coffee</NavLink>
-                <NavLink to={"/menu/hottea"}>Hot Tea</NavLink>
-                <NavLink to={"/menu/icetea"}>Ice Tea</NavLink>
-                <NavLink to={"/menu/ade&juice"}>Ade & Juice</NavLink>
-                <NavLink to={"/menu/smoothie&frappe"}>Smoothie & Frappe</NavLink>
-                <NavLink to={"/menu/hotdecaf"}>Hot Decaffein Coffee</NavLink>
-                <NavLink to={"/menu/icedecaf"}>Ice Decaffein Coffee</NavLink>
-                <NavLink to={"/menu/newdrinks"}>New Drinks</NavLink>
-                <NavLink to={"/menu/dessert"}>Dessert</NavLink>
-                <NavLink to={"/menu/newdessert"}>New Dessert</NavLink>
-=======
+
                 <NavLink to={"/hotcoffee"}>Hot Coffee</NavLink>
                 <NavLink to={"/icecoffee"}>Ice Coffee</NavLink>
                 <NavLink to={"/hottea"}>Hot Tea</NavLink>
@@ -30,7 +18,7 @@ const Nav = ()=>{
                 <NavLink to={"/dessert"}>Dessert</NavLink>
                 <NavLink to={"/newdessert"}>New Dessert</NavLink>
                 <NavLink to={"/purchase"}>결제</NavLink>
->>>>>>> afa7e9609a64e39c79242d1b4d6d2aa257dec432
+
             </ul>
         </div>
     )
