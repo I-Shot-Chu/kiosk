34f22a0a6f98c636ce3ee372f7ac321dc8093c61
Your Name <iamwon942@gmail.com>
2024-10-13 00:16:37 +0900

    누가 메뉴 수평 정렬좀..

--- src/features/ShowItems.js
+++ src/features/ShowItems.js
@@ -2,107 +2,51 @@ import Modal from "react-modal";
 import { useState } from "react";
 import MenuDetail from "../pages/option/ShowExtraOptions";
 import { language } from "../store/store";
-import './ShowItems.css'
+import './ShowItems.css';
+
+
+
+const MenuItem = ({ menu, type }) => {
+  const imageUrl = require(`../assets/images/images_menus/${menu.menuCode}.jpg`);
+  const [modal, setModal] = useState(false);
+  const { lang } = language();
+
+  const onClickHandler = () => setModal(true);
+  const onClickHandler2 = () => setModal(false);
+
+  return (
+    <>
+      <div className="item_button_container">
+        <button onClick={onClickHandler} className="menu_button">
+          <div className="menuItem">
+            <img src={imageUrl} alt={menu.menuName} width="100px" />
+            <h4>{lang ? menu.menuName : menu.enName}</h4>
+            <p>{menu.menuPrice}{lang ? "원" : " Won"}</p>
+          </div>
+        </button>
+      </div>
+      <Modal 
+        isOpen={modal} 
+        ariaHideApp={false} 
+        className={'menu_detail_modal'}
+      >
+        <MenuDetail menuCode={menu.menuCode} onClickHandler2={onClickHandler2} />
+      </Modal>
+    </>
+  );
+};
 
 // 음료
 export const DrinkMenu = ({ drinkMenu }) => {
-    const imageUrl = require(`../assets/images/images_menus/${drinkMenu.menuCode}.jpg`);
-    
-    const [modal, setModal] = useState(false);
-
-    const { lang } = language();
-
-    const onClickHandler = () =>
-    {
-        setModal(true);
-    }
-
-    const onClickHandler2 = () =>
-    {
-        setModal(false);
-    }
-
-    return (
-        <>
-            <button onClick={onClickHandler}>
-                <div className="DrinkMenuItem">
-                    <img src={imageUrl} alt={`${drinkMenu.menuName}`} width="100px" />
-                    <h4>{lang ? `${drinkMenu.menuName}` : `${drinkMenu.enName}`}</h4>
-                    <ul>{drinkMenu.menuPrice}{lang ? "원" : " Won"}</ul>
-                </div>
-            </button>
-            <Modal isOpen = {modal} ariaHideApp={false} shouldCloseOnOverlayClick = {false} className={'menu_detail_modal'}>
-                <MenuDetail menuCode = {drinkMenu.menuCode} onClickHandler2 = {onClickHandler2}/>
-            </Modal>
-        </>
-    );
+  return <MenuItem menu={drinkMenu} type="drink" />;
 };
 
 // 디저트
 export const DessertMenu = ({ dessertMenu }) => {
-
-    const imageUrl = require(`../assets/images/images_menus/${dessertMenu.menuCode}.jpg`);
-
-    const [modal, setModal] = useState(false);
-
-    const { lang } = language();
-
-    const onClickHandler = () =>
-    {
-        setModal(true);
-    }
-
-    const onClickHandler2 = () =>
-    {
-        setModal(false);
-    }
-
-    return (
-        <>
-            <button onClick={onClickHandler}>
-            <div className="DessertMenuItem">
-                <img src={imageUrl} alt={`${dessertMenu.menuName}`} width="100px" />
-                <h4>{lang ? `${dessertMenu.menuName}` : `${dessertMenu.enName}`}</h4>
-                <ul>{dessertMenu.menuPrice}{lang ? "원" : " Won"}</ul>
-            </div>
-            </button>
-            <Modal isOpen = {modal} ariaHideApp={false} shouldCloseOnOverlayClick = {false} className={'menu_detail_modal'}>
-                <MenuDetail menuCode = {dessertMenu.menuCode} onClickHandler2 = {onClickHandler2}/>
-            </Modal>
-        </>  
-    );
+  return <MenuItem menu={dessertMenu} type="dessert" />;
 };
 
 // MD 상품
 export const MdItem = ({ mdProduct }) => {
-    const imageUrl = require(`../assets/images/images_menus/${mdProduct.menuCode}.jpg`);
-
-    const [modal, setModal] = useState(false);
-
-    const { lang } = language();
-
-    const onClickHandler = () =>
-    {
-        setModal(true);
-    }
-
-    const onClickHandler2 = () =>
-    {
-        setModal(false);
-    }
-
-    return (
-        <>
-            <button onClick={onClickHandler}>
-            <div className="DessertMenuItem">
-                <img src={imageUrl} alt={`${mdProduct.menuName}`} width="100px" />
-                <h4>{lang ? `${mdProduct.menuName}` : `${mdProduct.enName}`}</h4>
-                <ul>{mdProduct.menuPrice}{lang ? "원" : " Won"}</ul>
-            </div>
-            </button>
-            <Modal isOpen = {modal} ariaHideApp={false} shouldCloseOnOverlayClick = {false} className={'menu_detail_modal'}>
-                <MenuDetail menuCode = {mdProduct.menuCode} onClickHandler2 = {onClickHandler2}/>
-            </Modal>
-        </>  
-    );
+  return <MenuItem menu={mdProduct} type="md" />;
 };
