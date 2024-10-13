b1c558bd2695fc3bcd364be80647b557798bd041
R-JANG-98 <msgpigeon@gmail.com>
2024-10-13 22:32:18 +0900

    category merge

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -9,21 +9,17 @@ const Nav = ()=>{
                 <NavLink to={"/menu/newdrinks"}>추천 음료/ </NavLink>
                 <NavLink to={"/menu/newdessert"}>추천 디저트/ </NavLink>
 
-                <NavLink to={"/menu/hotcoffee"}>커피(HOT)/ </NavLink>
-                <NavLink to={"/menu/icecoffee"}>커피(ICE)/ </NavLink>
+                <NavLink to={"/menu/hotcoffee"}>커피/ </NavLink>
 
-                <NavLink to={"/menu/hottea"}>티(HOT)/ </NavLink>
-                <NavLink to={"/menu/icetea"}>티(ICE)/ </NavLink>
+                <NavLink to={"/menu/hottea"}>티/ </NavLink>
 
                 <NavLink to={"/menu/ade&juice"}>에이드&주스/ </NavLink>
                 <NavLink to={"/menu/smoothie&frappe"}>스무디&프라페/ </NavLink>
+
+                <NavLink to={"/menu/hotdecaf"}>디카페인/ </NavLink>
+
+                <NavLink to={"/menu/hotdrinks"}>음료/ </NavLink>
                 <br></br>
-                <NavLink to={"/menu/hotdecaf"}>디카페인(HOT)/ </NavLink>
-                <NavLink to={"/menu/icedecaf"}>디카페인(ICE)/ </NavLink>
-
-                <NavLink to={"/menu/hotdrinks"}>음료(HOT)/ </NavLink>
-                <NavLink to={"/menu/icedrinks"}>음료(ICE)/ </NavLink>
-
                 <NavLink to={"/menu/dessert"}>디저트/ </NavLink>
                 <NavLink to={"/menu/md"}>상품/ </NavLink>
             </ul>
--- src/features/getMenuDetails.js
+++ src/features/getMenuDetails.js
@@ -5,54 +5,58 @@ import newMenu from "../json/newMenu.json"
 
 
 // 커피 
-const isHotCoffee = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(HOT) 커피"){return true}
+const isHotCoffee = (drinkMenus)=>
+{
+    if(drinkMenus.categoryName === "커피"){return true}
 }
-export const hotCoffee = ()=> { return drinkMenus.filter(isHotCoffee) }
+export const hotCoffee = () => { return drinkMenus.filter(isHotCoffee) }
 
-const isIceCoffee = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 커피"){return true}
+const isIceCoffee = (drinkMenus)=>
+{
+    if(drinkMenus.categoryName === "커피"){return true}
 }
-export const iceCoffee = ()=> { return drinkMenus.filter(isIceCoffee) }
+export const iceCoffee = () => { return drinkMenus.filter(isIceCoffee) }
 
 
 
 // 티
-const isHotTea = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(HOT) 티"){return true}
+const isHotTea = (drinkMenus)=>
+{
+    if(drinkMenus.categoryName === "티"){return true}
 }
-export const hotTea = ()=>{ return drinkMenus.filter(isHotTea) }
+export const hotTea = () => { return drinkMenus.filter(isHotTea) }
 
-const isIceTea = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 티"){return true}
+const isIceTea = (drinkMenus)=>
+{
+    if(drinkMenus.categoryName === "티"){return true}
 }
-export const iceTea = ()=>{ return drinkMenus.filter(isIceTea) }
+export const iceTea = () => { return drinkMenus.filter(isIceTea) }
 
 
 // 에이드, 주스 
 const isAdeAndJuice = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 에이드&주스"){return true}
+    if(drinkMenus.categoryName === "에이드&주스"){return true}
 }
-export const adeAndJuice = ()=>{ return drinkMenus.filter(isAdeAndJuice) }
+export const adeAndJuice = () => { return drinkMenus.filter(isAdeAndJuice) }
 
 
 
 // 스무디 , 프라페
 const isSmoothieAndFrappe = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 스무디 & 프라페"){return true}
+    if(drinkMenus.categoryName === "스무디 & 프라페"){return true}
 }
-export const smoothieAndFrappe = ()=>{ return drinkMenus.filter(isSmoothieAndFrappe) }
+export const smoothieAndFrappe = () => { return drinkMenus.filter(isSmoothieAndFrappe) }
 
 
 
 // 디카페인
 const isHotDecafCoffee = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(HOT) 디카페인"){return true}
+    if(drinkMenus.categoryName === "디카페인"){return true}
 }
 export const hotDecafCoffee = ()=>{ return drinkMenus.filter(isHotDecafCoffee) }
 
 const isIceDecafCoffee = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 디카페인"){return true}
+    if(drinkMenus.categoryName === "디카페인"){return true}
 }
 export const iceDecafCoffee = ()=>{ return drinkMenus.filter(isIceDecafCoffee) }
 
@@ -60,12 +64,12 @@ export const iceDecafCoffee = ()=>{ return drinkMenus.filter(isIceDecafCoffee) }
 
 //음료
 const isHotDrinks = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(HOT) 음료"){return true}
+    if(drinkMenus.categoryName === "음료"){return true}
 }
 export const hotDrinks = ()=>{ return drinkMenus.filter(isHotDrinks) }
 
 const isIceDrinks = (drinkMenus)=>{
-    if(drinkMenus.categoryName === "(ICE) 음료"){return true}
+    if(drinkMenus.categoryName === "음료"){return true}
 }
 export const iceDrinks = ()=>{ return drinkMenus.filter(isIceDrinks) }
 
